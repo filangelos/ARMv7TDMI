0c09db04afb609929347752551c140f68587dce4
BaronKhan <bksonic@hotmail.co.uk>
2017-02-23 14:45:50 +0000

    Update tokeniser

--- Assembler/Tokeniser.fs
+++ Assembler/Tokeniser.fs
@@ -7,7 +7,7 @@ module Tokeniser =
     
     //returns the first matched group of a regex and the leftovers from the input
     let (|MatchToken|_|) pattern input =
-        let m = Regex.Match(input,pattern) 
+        let m = Regex.Match(input, "(?<![\s\S]+)" + pattern) //pattern must start at beginning of string
         if (m.Success)
         then Some (m.Groups.[1].Value, (new Regex(pattern)).Replace(input, "", 1))
         else None
@@ -18,20 +18,23 @@ module Tokeniser =
         //breaks down a string into a list of tokens and appends them onto lst
         let rec strToToken (lst:Token list) (str:string) =
             match str with
-            | MatchToken "[rR]([0-9][0-6]?)(?![^,])" (reg, leftovers) ->
+            //str may contain several tokens, so recursively call MatchToken until str is "" 
+            | MatchToken "[rR]([0-9]|1[0-6])(?![^,])" (reg, leftovers) ->                           //register
                 strToToken (lst @ [TokReg(reg |> int)]) leftovers
-            | MatchToken "#0[xX]([0-9A-Fa-f]+(?![^0-9A-Fa-f,]))" (hexVal, leftovers) ->
+            | MatchToken "#(0[xX][0-9A-Fa-f]+(?![^0-9A-Fa-f,\[\]\{\}\!]))" (hexVal, leftovers) ->   //hex const
                 //printfn "hex: %A" hexVal
-                let intVal = System.Convert.ToInt32(hexVal, 16)
-                strToToken (lst @ [TokConst intVal]) leftovers
-            | MatchToken "#0[bB]([01]+(?![^01,]))" (binVal, leftovers) ->
+                //let intVal = System.Convert.ToInt32(hexVal, 16)
+                //strToToken (lst @ [TokConst intVal]) leftovers
+                strToToken (lst @ [TokConst (int hexVal)]) leftovers
+            | MatchToken "#(0[bB][01]+(?![^01,\[\]\{\}\!]))" (binVal, leftovers) ->                 //bin const
                 //printfn "bin: %A" binVal
-                let intVal = System.Convert.ToInt32(binVal, 2)
-                strToToken (lst @ [TokConst intVal]) leftovers
-            | MatchToken "#([0-9]+)(?![^0-9,])" (value, leftovers) ->
+                //let intVal = System.Convert.ToInt32(binVal, 2)
+                //strToToken (lst @ [TokConst intVal]) leftovers
+                strToToken (lst @ [TokConst (int binVal)]) leftovers
+            | MatchToken "#([0-9]+)(?![^0-9,\[\]\{\}\!])" (value, leftovers) ->                     //dec const
                 //printfn "dec: %A" value
                 strToToken (lst @ [TokConst(value |> int)]) leftovers
-            | MatchToken "((?<![0-9]+)[A-Za-z][A-Za-z0-9_]*)" (name, leftovers) ->
+            | MatchToken "((?<![0-9]+)[A-Za-z][A-Za-z0-9_]*)" (name, leftovers) ->                  //identifier (MOV, ADD, label)
                 strToToken (lst @ [TokIdentifier name]) leftovers
             | MatchToken "," (_, leftovers) ->
                 strToToken (lst @ [TokComma]) leftovers
@@ -50,9 +53,10 @@ module Tokeniser =
             | "" -> lst
             | _ -> failwithf "Unidentified character(s) in %A" str
 
-        let strList = input.Split([|' '; '\t'|])
 
+        let strList = input.Split([|' '; '\t'|])
+        printfn "%A" strList
         Array.fold strToToken [] strList
 
     let tokeniseTest =
-        printfn "%A" (tokenise "MOV R1, r2 \n LABEL \n ADD r1, r14, #0xF")
+        printfn "%A" (tokenise "MOV R1 ,r16 \n LABEL \n ADD [r23 ,#3]r1, r14 ,#0b101 \n LDR r0!, [r1, #0x5]")
