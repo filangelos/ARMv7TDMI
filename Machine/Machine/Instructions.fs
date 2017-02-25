8345fc0ace9d13881876299279724f911b5e3dcc
Youssef Rizkk <youssefarizk@gmail.com>
2017-02-25 10:39:31 +0000

    Updating Instructions.fs

---
+++ Machine/Machine/Instructions.fs
@@ -0,0 +1,58 @@
+namespace Machine
+
+module InstructionsModule =
+
+
+    open MachineState
+    open Common
+
+    // stortcuts
+    let ( ^. ) = Optics.get MachineState.Register_
+    let ( ^= ) = Optics.set MachineState.Register_
+    let ( ^* ) = Optics.get MachineState.Flag_
+    let ( ^- ) = Optics.set MachineState.Flag_
+
+    //Op2 can be either a register or a literal
+    //type OP2 = Reg of Register | Op of Data
+
+    let addWithCarryR ((regD: RegisterID), (regN: RegisterID), (op2: Operand), (state: MachineState))=
+        let regNValue = (^.) regN state
+
+        let op2Value = match op2 with 
+                       | Reg(register) -> (^.) register state
+                       | Op(data) -> data
+
+        let (carryVal: Data) = match ( ^* ) C state with
+                       |true -> regNValue + op2Value + Data 1
+                       |false -> regNValue + op2Value
+       
+
+        (^=) (regD) (carryVal) (state)
+
+    //let subtractWithCarry ((regD: RegisterID), (regN: RegisterID), (op2: RegisterID), (state: MachineState)) =
+
+
+    
+
+    //test code for add without carry
+    let a = MachineState.make()
+    let b = (^=) R0 1 a
+    let c = (^=) R1 2 b
+    let d = (^=) R2 3 c
+    let e = (^=) R3 4 d
+    let f = (^=) R4 5 e
+    let g = (^=) R5 6 f
+    let h = (^=) R6 57 g
+    let i = (^=) R7 23 h
+    let j = (^=) R8 67 i
+    let k = (^=) R9 34 j
+    let l = (^=) R10 24 k
+    let m = (^=) R11 7 l
+    let n = (^=) R12 9 m
+
+
+    let inputs = [(R0,R5,Op(7),n);(R0,R1,Reg(R2),n);(R10,R1,Reg(R5),n);(R4,R1,Reg(R7),n);(R5,R12,Reg(R2),n)]
+
+    let testOutput = List.map addWithCarryR inputs
+
+    printfn "%A" testOutput
