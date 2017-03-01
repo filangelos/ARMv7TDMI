b55809084340cdf0462287e4250708b39cc776fa
Youssef Rizkk <youssefarizk@gmail.com>
2017-03-01 16:25:13 +0000

    Added ability to setFlags within the same addWithCarryS function, thereby completing ADD, ADDS, ADC, ADCS instructions

--- ARM7TDMI/ARM7TDMI/Instructions.fs
+++ ARM7TDMI/ARM7TDMI/Instructions.fs
@@ -23,7 +23,7 @@ module Instructions =
     //NOTE: Check if V flag is set by shift operations
     //Checks overflow. Returns tuple containing value of addition (not used) and new state reflecting new overflow flag
     let setOverflow a b state =
-        try (Checked.(+) a b),state with
+        try (Checked.(+) a b),(( ^- ) V false state) with
         e -> 1,(( ^- ) V true state)
      
     //sets Zero flag. Returns a state
@@ -45,7 +45,7 @@ module Instructions =
     //Op2 can be either a register or a literal
     //type OP2 = Reg of Register | Op of Data
     //right now implementing adcs
-    let addWithCarry ((regD: RegisterID), (regN: RegisterID), (op2: Operand), (state: MachineState), (carry: bool))=
+    let addWithCarryS ((regD: RegisterID), (regN: RegisterID), (op2: Operand), (state: MachineState), (includeCarry: bool), (setFlags: bool))=
 
         //extracting operand values
         let regNValue = (^.) regN state
@@ -57,29 +57,34 @@ module Instructions =
 
         //including the value of carry into the first register and producing a new state : (newRegisterValue:Data,newState:MachineState)
         let newRegVal =
-            match carry && ( ^* ) C state with
+            match setFlags && includeCarry && ( ^* ) C state with
             |true ->  setCarry (+) regNValue (Data 1) state
+            |false when includeCarry && ( ^* ) C state -> fst (setCarry (+) regNValue (Data 1) state), state
             |false -> (regNValue, state)
        
         //updating the state encapsulation
         let state1 = snd newRegVal
 
         //Producing result of the operation, along with a state that reflects the change by the carry : (finalResult: Data, newState: MachineState)
-        let finVal = match ( ^* ) C state1 with
-                     |true -> (fst (setCarry (+) regNValue op2Value state1)),state1
-                     |false -> setCarry (+) regNValue op2Value state1
+        //Under correct execution, the C flag of state1 should only reflect
+        let finVal = match setFlags && ( ^* ) C state1 with
+                     |true |false when not setFlags -> (fst (setCarry (+) regNValue op2Value state1)),state1
+                     |false when setFlags -> setCarry (+) regNValue op2Value state1
+                     | _ -> failwithf "This will never happen"
 
         //updating the state encapsulation again     
         let state2 = snd finVal
 
         let result = fst finVal
 
-        let state3 = snd (setOverflow regNValue op2Value state2)
+        //Obtaining state reflecting signed overflow
+        let state3 = if setFlags then snd (setOverflow regNValue op2Value state2) else state2
 
-        let state4 = setNegative result state3
+        //Obtaining state reflecting sign of result
+        let state4 = if setFlags then setNegative result state3 else state3
 
-        //Obtaining final state reflecting signed overflow
-        let finState = setZero result state4
+        //Obtaining state reflecting zero status
+        let finState = if setFlags then setZero result state3 else state3
 
         (^=) (regD) (result) (finState)
 
@@ -90,17 +95,13 @@ module Instructions =
 
     //test code for add without carry
     let a = MachineState.make()
-    let b = (^=) R0 -2147483648 a
-    let c = (^=) R1 -2147483648 b
+    let b = (^=) R0 -1073741824 a
+    let c = (^=) R1 -268435456 b
     let d = ( ^- ) C false c
     let e = ( ^- ) V false d
     let f = ( ^- ) N false e
     let g = ( ^- ) Z false f
+    let h = addWithCarryS (R0,R0,ID(R0),g, false, true)
+    let i = addWithCarryS (R2,R0,ID(R1),h, true, true)
 
-
-
-    let inputs = [(R0,R0,ID(R1),g, false)]
-
-    let testOutput = List.map addWithCarry inputs
-
-    printfn "%A" testOutput
+    printfn "%A" i
