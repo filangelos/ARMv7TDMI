5ee01633832c07bd225f1acf14a23a8fc4ca80f1
Pranav <pranavbanesh@gmail.com>
2017-03-02 13:46:01 +0000

    Started on implementing Parsing and Active Pattern Matching

--- ARM7TDMI/ARM7TDMI/Parser.fs
+++ ARM7TDMI/ARM7TDMI/Parser.fs
@@ -7,8 +7,60 @@
     Contributors: Pranav Prabhu
 
     Module: Parser
-    Description: 
+    Description: Parse individual instruction and initiate correct function call
 *)
+open System
 
 module Parser =
-    let x = 0
+    let pchar(cToMatch,str) =
+        if String.IsNullOrEmpty(str) then
+            let msg = "No more input"
+            (msg,"")
+        else 
+            let first = str.[0] 
+            if first = cToMatch then
+                let remaining = str.[1..]
+                let msg = sprintf "Found %c" cToMatch
+                (msg,remaining)
+            else
+                let msg = sprintf "Expecting '%c'. Got '%c'" cToMatch first
+                (msg,str)
+        
+    let (|ADD|_|) rd rn op2 =
+
+    let (|ADDC|_|) rd rn op2 =
+
+    let (|ADDS|_|) rd rn op2 =
+
+    let (|AND|_|) rd rn op2 =
+
+    let (|ANDS|_|) rd rn op2 =
+
+    let (|ADD|_|) rd rn op2 =
+    
+
+    let instructionParse str  :Instructions = 
+        match str with
+        | ADD rd rn op2  -> addWithCarryS rd rn op2 MachineState false false
+        | ADDC rd rn op2  -> addWithCarryS rd rn op2 MachineState true false
+        | ADDS rd rn op2  -> addWithCarryS rd rn op2 MachineState false true
+        | ADDCS rd rn op2  -> addWithCarryS rd rn op2 MachineState true true
+        | AND rd rn op2  -> andOp rd rn op2 MachineState false
+        | ANDS rd rn op2  -> andOp rd rn op2 MachineState true
+        | MOV rd op2 -> mov rd op2 MachineState false
+        | MOVS rd op2 -> mov rd op2 MachineState true
+        | MVN rd op2 -> mvn rd op2 MachineState false
+        | MVNS rd op2 -> mvn rd op2 MachineState true
+        | OR rd rn op2 -> orr rd rn op2 MachineState false
+        | ORS rd rn op2 -> orr rd rn  op2 MachineState true
+        | XOR rd op2 -> eOr rd rn op2 MachineState false
+        | XORS rd op2 -> eOr rd rn op2 MachineState true
+        | OR rd rn op2 -> orr rd rn op2 MachineState false
+        | ORS rd rn op2 -> orr rd rn op2 MachineState true
+        | BIC rd rn op2 -> orr rd rn op2 MachineState false
+        | BICS rd rn op2 -> orr rd rn op2 MachineState true
+
+    let parseAndReturn (tokenList: Token List): Instructions List = 
+        tokenList |> instructionParse |> List.append
+
+    
