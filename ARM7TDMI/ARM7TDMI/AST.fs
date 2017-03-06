3068335223ff2f008915b93a74a58add5849b255
BaronKhan <bksonic@hotmail.co.uk>
2017-03-06 23:12:03 +0000

    Fix condition evaluation in AST

--- ARM7TDMI/ARM7TDMI/AST.fs
+++ ARM7TDMI/ARM7TDMI/AST.fs
@@ -23,34 +23,34 @@ module AST =
 
     ///different parameters based on instruction functions
     type Parameters =
-        | ParametersAdd of (RegisterID * RegisterID * Operand * MachineState * bool * bool * ShiftDirection)            //(regD, regN, op2, state, includeCarry, setFlags, ShiftDirection)
-        | ParametersSub of (RegisterID * RegisterID * Operand * MachineState * bool * bool * bool * ShiftDirection)     //(regD, regN, op2, state, includeCarry, reverse, setFlags, ShiftDirection)
-        | Parameters1RegShift of (RegisterID * Operand * MachineState * bool * ShiftDirection)                          //(regD, op2, state, setFlags, ShiftDirection)
-        | Parameters1Reg of (RegisterID * Operand * MachineState * bool)                                                //(regD, op2, state, setFlags)
-        | Parameters2Reg of (RegisterID * RegisterID * Operand * MachineState * bool)                                   //(regD, regN, op2, state, setFlags)
+        | ParametersAdd of (RegisterID * RegisterID * Operand * bool * bool * ShiftDirection)            //(regD, regN, op2, includeCarry, setFlags, ShiftDirection)
+        | ParametersSub of (RegisterID * RegisterID * Operand * bool * bool * bool * ShiftDirection)     //(regD, regN, op2, includeCarry, reverse, setFlags, ShiftDirection)
+        | Parameters1RegShift of (RegisterID * Operand * bool * ShiftDirection)                          //(regD, op2, setFlags, ShiftDirection)
+        | Parameters1Reg of (RegisterID * Operand * bool)                                                //(regD, op2, setFlags)
+        | Parameters2Reg of (RegisterID * RegisterID * Operand * bool)                                   //(regD, regN, op2, setFlags)
 
     ///type representing the memory location (an int value in bytes) of the instruction or data (incr. addr by 4 bytes for each instruction parsed).
     type Address = int                  //Replace int with MemoryLocation when Memory is done.
 
-    type Condition = bool
+    ///type used for specifying a conditional code. Usage: Z = 1 -> (Z, true); V = 0 -> (V, false)
+    type Condition = FlagID * bool
 
     ///type representing the possible nodes in the AST
-    type Node =
-        | InstructionNode of InstructionKeyword * Parameters * Condition * Address
+    type InstructionNode = InstructionKeyword * Parameters * (Condition option) * Address
 
     ///type representing the mapping of labels to memory addresses
     type LabelMap = Map<string, Address>
 
     ///type representing the AST (just a list of nodes as well as the map for the label mappings)
-    type AST = (Node list) * LabelMap
+    type AST = (InstructionNode list) * LabelMap
 
     ////Build the AST in the parser using these functions////
 
     ///adds an instruction node to the AST, address should be an int and assigned in the parser (increase addr by 4 bytes for each instr. parsed).
     ///usage: addInstructionNode ast addWithCarryS Parameters4(R1, R2, op2, MachineState, false, true) [-current address int value-]
-    let inline addInstructionNode (ast:AST) (f:InstructionKeyword) (p:Parameters) (c:Condition) (addr:Address) =
+    let inline addInstructionNode (ast:AST) (f:InstructionKeyword) (p:Parameters) (c:Condition option) (addr:Address) =
         match ast with
-        | lst, labelMap -> (lst @ [InstructionNode(f, p, c, addr)], labelMap)
+        | lst, labelMap -> (lst @ [(f, p, c, addr)], labelMap)
 
     ///adds a label node to the AST, address should be an int and assigned in the parser (increase addr by 4 bytes for each instr. parsed).
     ///usage: addLabelNode ast LABEL1 [-current address int value-]
@@ -58,14 +58,21 @@ module AST =
         match ast with
         | lst, labelMap -> (lst, labelMap.Add (name, addr))
 
+    ///evaluates a condtion of type Condition and returns true or false
+    let evaluateCondition (cond:Condition option) (state:MachineState) =
+        match cond with
+        | Some(fID, eq) ->
+            ( ^* ) fID state = eq
+        | None -> true
+        | _ -> failwithf "invalid condition"
 
     let rec reduce (ast:AST) (state:MachineState) =
         match ast with
-        | InstructionNode(f, p, cond, addr)::t, lst ->
-            if cond then
+        | (f, p, cond, addr)::t, lst ->
+            if evaluateCondition cond state then
                 match f, p with
-                | MOV, Parameters1RegShift(p) ->
-                    let newState = Instructions.mov p
+                | MOV, Parameters1RegShift((regD, op2, setFlags, shiftDirection)) ->
+                    let newState = Instructions.mov (regD, op2, state, setFlags, shiftDirection)
                     reduce (t,lst) newState
                 | _ -> failwithf "Could not execute instruction"
             else
@@ -74,24 +81,22 @@ module AST =
         | _ -> failwithf "Not a valid node"
 
     (*--------------------------------------------------------TESTING--------------------------------------------------------*)
+
     let testAST =
         printfn "Running testAST:"
-        let testState = MachineState.make()
-        printfn "empty machine state: %A" testState
-        let ast = ([], Map.empty<string, Address>)
-        let  myAst = addInstructionNode ast (MOV) (Parameters1RegShift(R1, Literal(13), testState, false, NoShift)) (true) 4
-        //printfn "ast is: %A" myAst
-        let result = reduce myAst testState
-        //printfn "new test state is %A" result
-        let  myAst2 = addInstructionNode myAst (MOV) (Parameters1RegShift(R2, Literal(342), result, false, NoShift)) (( ^. ) R1 result = 13) 4
-        printfn "ast is: %A" myAst2
-        let result2 = reduce myAst2 testState
-        printfn "new test state is %A" result2
+        let emptyState = MachineState.make()
+        printfn "empty machine state: %A" emptyState
+        let myAst1 = ([], Map.empty<string, Address>)
+        let  myAst2 = addInstructionNode myAst1 (MOV) (Parameters1RegShift(R1, Literal(0), true, NoShift)) (None) 2
+        let  myAst3 = addInstructionNode myAst2 (MOV) (Parameters1RegShift(R2, Literal(342), false, NoShift)) (Some(Z, true)) 4
+        printfn "ast is: %A" myAst3
+        let resultState2 = reduce myAst3 emptyState
+        printfn "new test state is %A" resultState2
         printfn "Finished testAST.\n"
-
 
     (*------------------------------------------------------------------------------------------------------------------------*)
     //IGNORE BELOW: if there's anything here, it should be removed
+    (*
 
 
 
@@ -101,8 +106,7 @@ module AST =
 
 
 
-
-
+    *)
     (*------------------------------------------------------------------------------------------------------------------------*)
 
     (* OLD AST - ONLY WORKS WITH ONE TYPE OF INSTRUCTION. This method was much more efficient so maybe come back to this method later. *)
