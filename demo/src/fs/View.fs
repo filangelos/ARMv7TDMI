b984c18c719664c889a30a7f4f121f176f3a6d5a
filangel <angelos.filos14@imperial.ac.uk>
2017-03-18 11:45:55 +0000

    syntax & dashboard

--- demo/src/fs/View.fs
+++ demo/src/fs/View.fs
@@ -31,7 +31,6 @@ module View =
 
     // Event Listeners
     let actions : unit -> unit = fun _ ->
-        // 
         // run button
         runBtn.onclick <- (fun _ -> console.log(window?code?getValue()); null)
         // theme button
@@ -43,42 +42,3 @@ module View =
             console.log("theme changed to: 'vs-dark'")
             window?code?updateOptions(options))
         ()
-
-    // Render Dashboard
-    let renderDashboard (state: MachineState) : string =
-        // render a single register
-        let renderReg ((id, content): RegisterID * Data) : string =
-              "<tr><td><div class=\"btn-group full-width\">"
-            + sprintf "<button id=\"%O\" class=\"btn btn-reg\">%O</button>" id?Case id?Case
-            + sprintf "<button class=\"btn btn-con\">%X</button>" content
-            + "<button class=\"btn btn-enc\">HEX</button>"
-            + "<button class=\"btn btn-enc\">BIN</button>"
-            + "<button class=\"btn btn-enc\">DEC</button>"
-            + "</div></td></tr>"
-
-        // core HTML
-        let core : string =
-            Optics.get MachineState.Registers_ state
-            |> Map.toList
-// This is necessary under Fable because in F# D.Us implement IComparable
-// according to the order the cases are defined, while with Fable they are
-// transpiled to a "Cases" object with string properties, therefore
-// they follow string comparison
-            |> List.map ( snd )
-            |> List.zip 
-                [ R0 ; R1 ; R2 ; R3 ; R4
-                  R5 ; R6 ; R7 ; R8 ; R9
-                  R10 ; R11 ; R12 ; R13 ; R14 ; R15 ]
-// End of "mess"
-            |> List.map renderReg
-            |> List.fold (+) ""
-        // glue everything together
-        [
-            "<table class=\"table text-center\"><tbody><tr><br></tr>"
-            core
-            "</tbody></table>"
-        ]
-        |> List.fold (+) ""
-
-    let initUI (state: MachineState) : unit =
-        dashboardDiv.innerHTML <- renderDashboard state
