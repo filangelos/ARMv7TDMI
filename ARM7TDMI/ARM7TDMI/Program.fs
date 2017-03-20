c19b23b99dcec3861b77db1d1ff7ba280fe7492c
filangel <angelos.filos14@imperial.ac.uk>
2017-03-20 00:02:15 +0000

    testing

--- ARM7TDMI/ARM7TDMI/Program.fs
+++ ARM7TDMI/ARM7TDMI/Program.fs
@@ -9,6 +9,7 @@ module Program =
     open Parser
     open MemoryInstructions
     open InstructionsInterfaces
+    open Expecto
 
     [<EntryPoint>]
     let main argv =
@@ -16,16 +17,17 @@ module Program =
     //    MemoryInstructions.simpleLDRSTRtest
 //      AST.testAST ()
 //        Parser.testParser ()
-        InstructionsInterfaces.test()
+(*        InstructionsInterfaces.test()
         let x: MachineState = MachineState.make ()
         let y: Data = (^.) R0 x
         let z: MachineState = (^=) R0 5 x
         let u: MachineState = Optics.set MachineState.Flag_ N true z
-        printfn "%A" x
+        printfn "%A" x *)
 
 
-        printfn "Testing pipeline:"
+(*        printfn "Testing pipeline:"
         let testInput = "MOV R0, #5\nMOV R1, #0x9\nADD R2, R1, R0\nSUBS R3, R0, R1"
         let result = (testInput |> tokenise |> Parse |> buildAST |> init |> execute)
-        printfn "%A \n\n These instructions give the following final state:\n%A" testInput result
-        0
+        printfn "%A \n\n These instructions give the following final state:\n%A" testInput result *)
+
+        runTests defaultConfig (testList "Complete Test" [ testMemory ])
