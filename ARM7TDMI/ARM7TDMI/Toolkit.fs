02cf4bd2aa77b27218949507b1f48373240b455b
filangel <angelos.filos14@imperial.ac.uk>
2017-03-23 16:16:11 +0000

    complete Toolkit.fs test

--- ARM7TDMI/ARM7TDMI/Toolkit.fs
+++ ARM7TDMI/ARM7TDMI/Toolkit.fs
@@ -13,6 +13,8 @@
 [<AutoOpen>]
 module Toolkit =
 
+    open Expecto
+
     /// enumerate all values of a D.U. 
     let inline enumerator<'T> =
         FSharp.Reflection.FSharpType.GetUnionCases(typeof<'T>)
@@ -40,3 +42,29 @@ module Toolkit =
               yield! loop (h::acc) t }
           
         loop [] lst |> List.ofSeq
+
+(*----------------------------------------------------------- Testing -----------------------------------------------------------*)
+
+    // initialise sandbox Type Dummy
+    type private Dummy = | X | Y | Z | A | B
+
+    /// Complete Module Unit Testing
+    let testToolbox : Test =
+
+        // enumerator
+        let testEnumerator : Test =
+            test "Enumerator" { Expect.equal enumerator<Dummy> [|X;Y;Z;A;B|] "should be [|X;Y;Z;A;B|]" }
+
+        // byte Optics test
+        let testSplitBy : Test =
+            testList "SplitBy" 
+                [ test "remove <- true" 
+                    { let dl : int list = [ 1 ; 13 ; 8 ; 0 ; 3 ; 4 ; 0 ; 1 ]
+                      let el : int list list = [ [ 1 ; 13 ; 8 ] ; [ 3 ; 4 ] ; [ 1 ] ]
+                      Expect.equal (splitBy 0 dl true) el "should be [ [ 1 ; 13 ; 8 ] ; [ 3 ; 4 ] ; [ 1 ] ]" }
+                  test "remove <- false"
+                    { let dl : int list = [ 1 ; 13 ; 8 ; 0 ; 3 ; 4 ; 0 ; 1 ]
+                      let el : int list list = [ [ 1 ; 13 ; 8 ; 0 ] ; [ 3 ; 4 ; 0 ] ; [ 1 ] ]
+                      Expect.equal (splitBy 0 dl false) el "should be [ [ 1 ; 13 ; 8 ; 0 ] ; [ 3 ; 4 ; 0 ] ; [ 1 ] ]" } ] 
+
+        testList "Top Level" [ testSplitBy ; ]
